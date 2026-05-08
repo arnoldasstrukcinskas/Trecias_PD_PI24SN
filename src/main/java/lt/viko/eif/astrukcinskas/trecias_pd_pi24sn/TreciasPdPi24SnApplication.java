c9f7118f3bf8c33300c680cb03d6cc004f010eb3
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2026-05-08 12:11:27 +0300

    Initial commit

---
+++ src/main/java/lt/viko/eif/astrukcinskas/trecias_pd_pi24sn/TreciasPdPi24SnApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.astrukcinskas.trecias_pd_pi24sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreciasPdPi24SnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreciasPdPi24SnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/viko/eif/astrukcinskas/trecias_pd_pi24sn/TreciasPdPi24SnApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.astrukcinskas.trecias_pd_pi24sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreciasPdPi24SnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
